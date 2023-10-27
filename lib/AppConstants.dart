f8399340995296c4dfb90d79b75bfaed8fbfbecd
Hamza Iddaoui <hamzaiddaoui99@gmail.com>
2023-10-27 18:47:26 +0200

    Map UI

--- lib/AppConstants.dart
+++ lib/AppConstants.dart
@@ -4,7 +4,7 @@ class AppConstants
 {
   static const String mapBoxAccessToken = 'pk.eyJ1IjoiaWtyYW1ldHUiLCJhIjoiY2xvNDRlbWw2MDljazJqcDA5dGxzbnJ1bSJ9.oTgLgAcZbjTZQ98qm-a5sw';
 
-  static const String mapBoxStyleId = 'clo47vr1500b001pg5czyfaq1';
+  static const String mapBoxStyleId = 'clo68wrvl00sx01qs0i6keihg';
 
-  static final myLocation = LatLng(51.5090214, -0.1982948);
+  static final myLocation = LatLng(33.699010, -7.383426);
 }
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,37 +1,119 @@
+
 import 'package:firstflutter/AppConstants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+CircleMarker perimetrecircle = CircleMarker(point: LatLng(33.699010, -7.383426), radius: 150, color: Colors.lightBlue.withOpacity(0.2));
+List<Marker> markers = [];
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  MyPage createState() => MyPage();
+}
+
+class MyPage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 33, 32, 32),
-        title: const Text('Flutter MAP BDD AVANCEE'),
+        title: const Text('Public Transportation'),
       ),
       body: Stack(
         children: [
           FlutterMap(
             options: MapOptions(
-              minZoom: 5,
-              maxZoom: 18,
-              zoom: 13,
+              zoom: 17,
               center: AppConstants.myLocation,
             ),
             layers: [
               TileLayerOptions(
                 urlTemplate:
-                "https://api.mapbox.com/styles/v1/ikrametu/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
-                additionalOptions: {
-                  'mapStyleId': AppConstants.mapBoxStyleId,
-                  'accessToken': AppConstants.mapBoxAccessToken,
-                },
+                "https://api.mapbox.com/styles/v1/ikrametu/clo68wrvl00sx01qs0i6keihg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWtyYW1ldHUiLCJhIjoiY2xvNDRldmMxMDhqNjJycDBsdGNmYjZwOCJ9.qUvQx-JPxdPS39ECpDoecw",
+                
               ),
+              CircleLayerOptions(
+                circles: [
+                  perimetrecircle
+                ]
+              ),
+              MarkerLayerOptions(
+                markers: [
+                  Marker(point: LatLng(33.699010, -7.382500), builder: (context) => 
+                   Image.asset('assets/map_marker.png', width: 60, height: 60)
+                  ),
+                  Marker(point: LatLng(33.699010, -7.383426), builder: (context) => 
+                   const Icon(Icons.directions_car_filled, color: Colors.black, size: 30.0,)
+                  ),
+                  Marker(point: LatLng(33.700000, -7.383426), builder: (context) => 
+                   const Icon(Icons.directions_car_filled, color: Colors.black, size: 30.0,)
+                  ),
+                  Marker(point: LatLng(33.698500, -7.383440), builder: (context) => 
+                   const Icon(Icons.directions_car_filled, color: Colors.black, size: 30.0,)
+                  ),
+                  Marker(point: LatLng(33.700100, -7.384000), builder: (context) => 
+                   const Icon(Icons.directions_bus_filled, color: Colors.lightGreen, size: 30.0,)
+                  ),
+                  Marker(point: LatLng(33.699010, -7.382000), builder: (context) => 
+                   const Icon(Icons.directions_bus_filled, color: Colors.lightGreen, size: 30.0,)
+                  ),
+                  Marker(point: LatLng(33.698500, -7.382000), builder: (context) => 
+                   const Icon(Icons.directions_train_outlined, color: Colors.redAccent, size: 30.0,)
+                  ),
+                ], 
+              )
             ],
           ),
+          const Column(
+            children: [
+            
+            ],
+          )
         ],
       ),
     );
   }
 }
+
+class FilterForm extends StatefulWidget{
+  const FilterForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return FilterFromState();
+  }
+
+}
+
+class FilterFromState extends State<FilterForm>{
+  final _formKey = GlobalKey<FilterFromState>();
+  double perimetre = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextField(
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              setState(() {
+                perimetre = double.tryParse(value) ?? 0;
+              });
+            },
+          ),
+          ElevatedButton(onPressed: () {
+            perimetrecircle = CircleMarker(point: perimetrecircle.point, radius: perimetre);
+          },
+          child: const Text("Submit"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
